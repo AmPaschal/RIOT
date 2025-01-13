5b4c0a30278e1070eefbbc6036c04b4054da9d22
Paschal Amusuo <amusuopaschal@gmail.com>
2025-01-13 16:46:05 -0500

    Created a new base projects for CBMC proofs.

---
+++ cbmc/proofs/harness_base/function_name_harness.c
@@ -0,0 +1,28 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file _rbuf_add_harness.c
+ * @brief Implements the proof harness for _rbuf_add function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+/**
+ * @brief Starting point for formal analysis
+ * 
+ */
+void harness(void)
+{
+
+  /* Insert argument declarations */
+
+}
