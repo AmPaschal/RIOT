2de3892e22d7960c2fcca1d969acff329aad918a
tlelievre26 <jackrabbit5567@gmail.com>
2025-01-15 14:42:25 -0500

    Step 2

---
+++ cbmc/proofs/gcoap_dns_server_proxy_get/gcoap_dns_server_proxy_get_harness.c
@@ -0,0 +1,39 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file _rbuf_add_harness.c
+ * @brief Implements the proof harness for _rbuf_add function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+/**
+ * @brief Starting point for formal analysis
+ * 
+ */
+
+#include <stdlib.h>
+
+void harness(void)
+{
+    size_t str_len;
+    __CPROVER_assume(str_len <= 20);
+
+    char* str = malloc(sizeof(char) * str_len);
+    __CPROVER_assume(str != NULL);
+    __CPROVER_assume(str[str_len - 1] == '\0');
+
+    gcoap_dns_server_proxy_get(str, str_len);
+  /* Insert argument declarations */
+
+
+}
