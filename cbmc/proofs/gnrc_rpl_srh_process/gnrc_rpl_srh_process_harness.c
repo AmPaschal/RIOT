1e3ce3056050404b2f85fec4550a7df31801ff54
OwenCochell <owencochell@gmail.com>
2025-01-25 17:23:24 -0500

    Bootstrap

---
+++ cbmc/proofs/gnrc_rpl_srh_process/gnrc_rpl_srh_process_harness.c
@@ -0,0 +1,14 @@
+/**
+ * @file _rbuf_add_harness.c
+ * @brief Implements the proof harness for _rbuf_add function.
+ */
+
+#include "net/gnrc/rpl/srh.h"
+
+/**
+ * @brief Starting point for formal analysis
+ * 
+ */
+void harness(void)
+{
+}
