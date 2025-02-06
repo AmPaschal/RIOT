106a2c0219e46151f1c0fe2c9ab3b24ffe37b74f
OwenCochell <owencochell@gmail.com>
2025-02-06 01:05:38 -0500

    Modeling input arguments

--- cbmc/proofs/_parse_options/_parse_options_harness.c
+++ cbmc/proofs/_parse_options/_parse_options_harness.c
@@ -3,10 +3,26 @@
  * @brief Implements the proof harness for _parse_options function.
  */
 
+#include <stdint.h>
+
+#include "net/gnrc/rpl/structs.h"
+
 /**
  * @brief Starting point for formal analysis
  * 
  */
 void harness(void)
 {
+    // Model input arguments
+    int msg_type;
+
+    gnrc_rpl_instance_t inst;
+    gnrc_rpl_opt_t opt;
+
+    ipv6_addr_t src;
+
+    uint16_t len;
+    uint32_t included_opts;
+
+    bool res = _parse_options(msg_type, &inst, &opt, len, &src, &included_opts);
 }
