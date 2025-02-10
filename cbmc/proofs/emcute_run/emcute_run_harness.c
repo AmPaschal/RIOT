a5a2e1162be96c8bd8984cf7a79fd84e2cc7d4ff
tlelievre26 <jackrabbit5567@gmail.com>
2025-02-10 11:00:01 -0500

    Step 6

--- cbmc/proofs/emcute_run/emcute_run_harness.c
+++ cbmc/proofs/emcute_run/emcute_run_harness.c
@@ -23,15 +23,30 @@
 
 #include <stdlib.h>
 
+#include "net/emcute.h"
+
+
+extern emcute_sub_t *subs;
+
+void emcute_callback(const emcute_topic_t *topic, void *data, size_t len) {
+    return;
+}
 
 void harness(void)
 {
     uint16_t port;
 
     size_t str_size;
-    __CPROVER_assume(str_size <= 20);
+
+    __CPROVER_assume(str_size <= 20 && str_size > 0);
     char* id = malloc(str_size);
     __CPROVER_assume(id != NULL);
+    __CPROVER_assume(id[str_size - 1] == '\0');
+
+    subs = malloc(sizeof(emcute_sub_t));
+    __CPROVER_assume(subs != NULL);
+    subs -> next = NULL;
+    subs -> cb = emcute_callback;
 
     emcute_run(port, id);
 }
