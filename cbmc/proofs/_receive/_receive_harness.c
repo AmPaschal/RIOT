f32ee9f4146188bdad2798caa4569d789d4a5b80
tlelievre26 <jackrabbit5567@gmail.com>
2025-02-17 12:35:05 -0500

    Resolved false positives

--- cbmc/proofs/_receive/_receive_harness.c
+++ cbmc/proofs/_receive/_receive_harness.c
@@ -23,6 +23,79 @@
 
 #include <stdlib.h>
 #include "net/gnrc/pkt.h"
+#include "gnrc_tcp_common.h"
+#include "net/tcp.h"
+
+_gnrc_tcp_common_tcb_list_t *_gnrc_tcp_common_get_tcb_list(void)
+{
+    _gnrc_tcp_common_tcb_list_t* head = malloc(sizeof(_gnrc_tcp_common_tcb_list_t));
+    __CPROVER_assume(head != NULL);
+    gnrc_tcp_tcb_t* tcb = malloc(sizeof(gnrc_tcp_tcb_t));
+    __CPROVER_assume(tcb != NULL);
+
+    tcb -> next = NULL;
+
+    head -> head = tcb;
+    return head;
+}
+
+gnrc_pktsnip_t *gnrc_pktbuf_start_write(gnrc_pktsnip_t *pkt)
+{
+    gnrc_pktsnip_t* pkt = malloc(sizeof(gnrc_pktsnip_t));
+    if(pkt == NULL) {
+        return pkt;
+    }
+
+    size_t size;
+    __CPROVER_assume(size <= 100);
+    uint8_t* data = malloc(size);
+    __CPROVER_assume(data != NULL);
+
+    pkt -> data = data;
+    pkt -> size = size;
+    pkt -> next = NULL;
+
+    return pkt;
+}
+
+gnrc_pktsnip_t *gnrc_pktbuf_mark(gnrc_pktsnip_t *pkt, size_t size, gnrc_nettype_t type)
+{
+    gnrc_pktsnip_t* pkt = malloc(sizeof(gnrc_pktsnip_t));
+    if(pkt == NULL) {
+        return pkt;
+    }
+
+    tcp_hdr_t* data = malloc(sizeof(tcp_hdr_t));
+    __CPROVER_assume(data != NULL);
+
+    pkt -> data = data;
+    pkt -> size = size;
+    pkt -> next = NULL;
+
+    return pkt;
+}
+
+gnrc_pktsnip_t *gnrc_pktsnip_search_type(gnrc_pktsnip_t *pkt,
+                                         gnrc_nettype_t type)
+{
+    gnrc_pktsnip_t* pkt = malloc(sizeof(gnrc_pktsnip_t));
+    if(pkt == NULL) {
+        return pkt;
+    }
+
+    size_t size;
+    __CPROVER_assume(size <= 100);
+    uint8_t* data = malloc(size);
+    __CPROVER_assume(data != NULL);
+
+    pkt -> data = data;
+    pkt -> size = size;
+    pkt -> type = type;
+    pkt -> next = NULL;
+
+    return pkt;
+}
+
 
 void harness(void)
 {
