c01cf914d9b504928a0198e55938ca0da513d465
tlelievre26 <jackrabbit5567@gmail.com>
2025-02-17 11:34:06 -0500

    Removed false positives

--- cbmc/proofs/_on_regack/_on_regack_harness.c
+++ cbmc/proofs/_on_regack/_on_regack_harness.c
@@ -24,15 +24,27 @@
 #include <stdlib.h>
 #include "net/asymcute.h"
 
+void user_cb(asymcute_req_t *req, unsigned evt_type) {
+    return;
+}
 
 void harness(void)
 {
     asymcute_con_t *con = malloc(sizeof(asymcute_con_t));
     __CPROVER_assume(con != NULL);
+    con -> user_cb = user_cb;
 
     asymcute_req_t *pending = malloc(sizeof(asymcute_req_t ));
     __CPROVER_assume(pending != NULL);
     pending -> next = NULL;
+
+    asymcute_topic_t* topic = malloc(sizeof(asymcute_topic_t));
+
+    // This is a void pointer that is cast to asymcute_topic_t
+    // On paper this could cause an OOB read after the cast
+    // But I'm fairly certain this struct is program-controlled
+    pending -> arg = topic;
+
     con -> pending = pending;
 
     asymcute_sub_t *subscriptions = malloc(sizeof(asymcute_req_t));
