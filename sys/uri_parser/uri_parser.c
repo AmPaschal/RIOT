7873f772876a5d6fa023277006835f0ceb88524a
Paschal Amusuo <amusuopaschal@gmail.com>
2025-02-21 13:17:41 -0500

    Completed harness

--- sys/uri_parser/uri_parser.c
+++ sys/uri_parser/uri_parser.c
@@ -60,8 +60,8 @@ static const char *_consume_scheme(uri_parser_result_t *result, const char *uri,
     result->scheme_len = p - uri;
 
     /* check if authority part exists '://' */
-    if ((p[1] != '\0') && (p[2] != '\0') && (p[1] == '/') && (p[2] == '/')) {
-    // if (((uri_end - p) > 2) && (p[1] == '/') && (p[2] == '/')) {
+    // if ((p[1] != '\0') && (p[2] != '\0') && (p[1] == '/') && (p[2] == '/')) {
+    if (((uri_end - p) > 2) && (p[1] == '/') && (p[2] == '/')) {
         *has_authority = true;
         /* skip '://' */
         return p + 3;
