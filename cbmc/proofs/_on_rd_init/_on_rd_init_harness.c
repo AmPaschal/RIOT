696c2514432ac85d3fc1a230e71f8cf811e14cbe
tlelievre26 <jackrabbit5567@gmail.com>
2025-05-06 14:53:24 -0400

    _on_rd_init precondition 3

--- cbmc/proofs/_on_rd_init/_on_rd_init_harness.c
+++ cbmc/proofs/_on_rd_init/_on_rd_init_harness.c
@@ -33,16 +33,19 @@ static coap_pkt_t* alloc_coap_pkt() {
     //Paschal's implementation
 
     coap_pkt_t* pkt = malloc(sizeof(coap_pkt_t));
-    __CPROVER_assume(pkt != NULL);
-    uint8_t pkt_size;
-    __CPROVER_assume(pkt_size > sizeof(coap_hdr_t));
-    uint8_t *hdr = malloc(pkt_size);
-    __CPROVER_assume(hdr != NULL);
-    pkt->hdr = hdr;
-    uint8_t payload_offset;
-    __CPROVER_assume(payload_offset >= sizeof(coap_hdr_t));
-    pkt->payload = hdr + payload_offset;
-    pkt->payload_len = pkt_size - payload_offset;
+    // __CPROVER_assume(pkt != NULL);
+    if (pkt != NULL) {
+        uint8_t pkt_size;
+        __CPROVER_assume(pkt_size > sizeof(coap_hdr_t));
+        uint8_t *hdr = malloc(pkt_size);
+        __CPROVER_assume(hdr != NULL);
+        pkt->hdr = hdr;
+        uint8_t payload_offset;
+        __CPROVER_assume(payload_offset >= sizeof(coap_hdr_t) && payload_offset <= pkt_size);
+        pkt->payload = hdr + payload_offset;
+        pkt->payload_len = pkt_size - payload_offset;
+    }
+
 
     return pkt;
 }
