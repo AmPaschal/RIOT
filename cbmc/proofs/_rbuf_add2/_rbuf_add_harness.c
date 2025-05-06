6250b7b7caa794f8d7255eda7e435c7f9eab5b0b
tlelievre26 <jackrabbit5567@gmail.com>
2025-05-06 14:57:18 -0400

    _rbuf_add precondition 2

--- cbmc/proofs/_rbuf_add2/_rbuf_add_harness.c
+++ cbmc/proofs/_rbuf_add2/_rbuf_add_harness.c
@@ -115,7 +115,7 @@ void harness(void)
     __CPROVER_assume(offset < 1000);
     // Adding this check to ensure consistency and remove an error.
     // We need to check if it is possible for a caller to violate this condition. If it is, it is a bug.
-    // __CPROVER_assume(offset != 0 || sixlowpan_frag_1_is(pkt->data) || sixlowpan_sfr_rfrag_is(data));
+    __CPROVER_assume(offset != 0 || sixlowpan_frag_1_is(pkt->data) || sixlowpan_sfr_rfrag_is(data));
     unsigned page;
 
     // Modeling rbuf
@@ -133,7 +133,7 @@ void harness(void)
     entry.super.datagram_size = datagram_size;
     // Added this to fix a bug. Not sure how best to model it
     int8_t offset_diff;
-    __CPROVER_assume(offset_diff >= 0); // Basically, offset diff should not be negative that it overflows.
+    // __CPROVER_assume(offset_diff >= 0); // Basically, offset diff should not be negative that it overflows.
     entry.offset_diff = offset_diff;
 
     entry.super.ints = malloc(sizeof(gnrc_sixlowpan_frag_rb_int_t));
