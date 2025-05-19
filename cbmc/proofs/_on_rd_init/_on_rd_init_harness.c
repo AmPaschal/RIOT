17ccc0fcf6e121a6a16e831df0e9d9566879b308
tlelievre26 <jackrabbit5567@gmail.com>
2025-05-19 17:38:15 -0400

    _on_rd_init precondition 1

--- cbmc/proofs/_on_rd_init/_on_rd_init_harness.c
+++ cbmc/proofs/_on_rd_init/_on_rd_init_harness.c
@@ -39,7 +39,7 @@ void harness(void)
     uint8_t pkt_size;
     __CPROVER_assume(pkt_size > sizeof(coap_hdr_t));
     uint8_t *hdr = malloc(pkt_size);
-    __CPROVER_assume(hdr != NULL);
+    // __CPROVER_assume(hdr != NULL);
     pkt->hdr = hdr;
     uint8_t payload_offset;
     __CPROVER_assume(payload_offset >= sizeof(coap_hdr_t) && payload_offset <= pkt_size);
